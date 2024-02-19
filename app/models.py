6efce95254d118c532897475a3205f861440e552
riccileonardo <122654112+riccileonardo@users.noreply.github.com>
2024-02-18 20:42:02 -0400

    Models (USER)
    Criando a model de USERS

--- app/models.py
+++ app/models.py
@@ -9,3 +9,17 @@ curso_trilha_association = Table('Curso_Trilha', Base.metadata,
     Column('Cursos_id', Integer, ForeignKey('Cursos.id')),
     Column('Trilha_apredizado_id', Integer, ForeignKey('Trilhas_apredizado.id'))
 )
+
+class User(Base):
+    __tablename__ = 'User'
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    username = Column(String(255), nullable=False)
+    password = Column(String(255), nullable=False)
+    email = Column(String(255), nullable=False)
+
+    def serialize(self):
+        return {
+            'id': self.id,
+            'username': self.username,
+            'email': self.email
+        }
