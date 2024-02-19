25d0c1b0b68a3d591c7cf327a2e49711cef5b834
riccileonardo <122654112+riccileonardo@users.noreply.github.com>
2024-02-18 21:01:23 -0400

    Criando meus edpoints
    Criando os Endpoints.

--- app/routes.py
+++ app/routes.py
@@ -12,3 +12,129 @@ jwt = JWTManager(app)
 
 bp = Blueprint('bp', __name__)
 Session = sessionmaker(bind=engine)
+
+# User
+@bp.route('/api/users/registro', methods=['POST'])
+def registro_usuario():
+    pass
+
+@bp.route('/api/users/login', methods=['POST'])
+def login_usuario():
+    pass
+
+@bp.route('/api/users/<int:id_user>', methods=['PUT'])
+def editar_usuario(id_user):
+    pass
+
+@bp.route('/api/users/<int:id_user>', methods=['DELETE'])
+def deletar_usuario(id_user):
+    pass
+
+@bp.route('/api/users', methods=['GET'])
+def visualizar_todos_usuarios():
+    pass
+
+@bp.route('/api/users/<int:id_user>', methods=['GET'])
+def visualizar_usuario_especifico(id_user):
+    pass
+
+# Curso
+@bp.route('/api/cursos', methods=['POST'])
+def cadastro_curso():
+    pass
+
+@bp.route('/api/cursos/<int:id_curso>', methods=['PUT'])
+def editar_curso(id_curso):
+    pass
+
+@bp.route('/api/cursos', methods=['GET'])
+def visualizar_todos_cursos():
+    pass
+
+@bp.route('/api/cursos/<int:id_curso>', methods=['GET'])
+def visualizar_curso_especifico(id_curso):
+    pass
+
+@bp.route('/api/cursos/<int:id_curso>', methods=['DELETE'])
+def deletar_curso(id_curso):
+    pass
+
+# Aula
+@bp.route('/api/cursos/<int:id_curso>/aulas', methods=['POST'])
+def cadastrar_aula(id_curso):
+    pass
+
+@bp.route('/api/cursos/<int:id_curso>/aulas/<int:id_aula>', methods=['PUT'])
+def atualizar_aula(id_curso, id_aula):
+    pass
+
+@bp.route('/api/cursos/<int:id_curso>/aulas', methods=['GET'])
+def visualizar_todas_aulas(id_curso):
+    pass
+
+@bp.route('/api/cursos/<int:id_curso>/aulas/<int:id_aula>', methods=['GET'])
+def visualizar_aula_especifica(id_curso, id_aula):
+    pass
+
+@bp.route('/api/cursos/<int:id_curso>/aulas/<int:id_aula>', methods=['DELETE'])
+def deletar_aula(id_curso, id_aula):
+    pass
+
+# Comentário
+@bp.route('/api/cursos/<int:id_curso>/comentarios', methods=['POST'])
+def cadastrar_comentario(id_curso):
+    pass
+
+@bp.route('/api/cursos/<int:id_curso>/comentarios/<int:id_comentario>', methods=['PUT'])
+def editar_comentario(id_curso, id_comentario):
+    pass
+
+@bp.route('/api/cursos/<int:id_curso>/comentarios', methods=['GET'])
+def visualizar_todos_comentarios(id_curso):
+    pass
+
+@bp.route('/api/cursos/<int:id_curso>/comentarios/<int:id_comentario>', methods=['DELETE'])
+def deletar_comentario(id_curso, id_comentario):
+    pass
+
+# Avaliação
+@bp.route('/api/cursos/<int:id_curso>/avaliacoes', methods=['POST'])
+def cadastrar_avaliacao(id_curso):
+    pass
+
+@bp.route('/api/cursos/<int:id_curso>/avaliacoes/<int:id_avaliacao>', methods=['PUT'])
+def atualizar_avaliacao(id_curso, id_avaliacao):
+    pass
+
+@bp.route('/api/cursos/<int:id_curso>/avaliacoes', methods=['GET'])
+def visualizar_todas_avaliacoes(id_curso):
+    pass
+
+@bp.route('/api/cursos/<int:id_curso>/avaliacoes/<int:id_avaliacao>', methods=['GET'])
+def visualizar_avaliacao_especifica(id_curso, id_avaliacao):
+    pass
+
+@bp.route('/api/cursos/<int:id_curso>/avaliacoes/<int:id_avaliacao>', methods=['DELETE'])
+def deletar_avaliacao(id_curso, id_avaliacao):
+    pass
+
+# Trilha de Aprendizado
+@bp.route('/api/trilhas-aprendizado', methods=['POST'])
+def cadastrar_trilha():
+    pass
+
+@bp.route('/api/trilhas-aprendizado/<int:id_trilha>', methods=['PUT'])
+def atualizar_trilha(id_trilha):
+    pass
+
+@bp.route('/api/trilhas-aprendizado', methods=['GET'])
+def visualizar_todas_trilhas():
+    pass
+
+@bp.route('/api/trilhas-aprendizado/<int:id_trilha>', methods=['GET'])
+def visualizar_trilha_especifica(id_trilha):
+    pass
+
+@bp.route('/api/trilhas-aprendizado/<int:id_trilha>', methods=['DELETE'])
+def deletar_trilha(id_trilha):
+    pass
